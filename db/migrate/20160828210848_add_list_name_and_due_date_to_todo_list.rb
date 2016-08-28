ca3f99bef217b9a2481c66dd176303923075e93c
Andrew Walters <andrew@andrewwalters.me>
2016-08-28 22:41:09 +0000

    Completed requirements 1 - 9

---
+++ db/migrate/20160828210848_add_list_name_and_due_date_to_todo_list.rb
@@ -0,0 +1,6 @@
+class AddListNameAndDueDateToTodoList < ActiveRecord::Migration
+  def change
+    add_column :todo_lists, :list_name, :string
+    add_column :todo_lists, :list_due_date, :date    
+  end
+end
---
+++ db/migrate/20160828215049_todo_item_description_to_text.rb
@@ -0,0 +1,8 @@
+class TodoItemDescriptionToText < ActiveRecord::Migration
+  def up
+    change_column :todo_items, :description, :text
+  end
+  def down
+    change_column :todo_items, :description, :string
+  end
+end
